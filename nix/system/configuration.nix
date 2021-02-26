1990f4f6a695888ab29b7342b3b7881879134eea
Ben Weintraub <ben@weintraub.xyz>
2021-02-25 21:14:10 -0500

    [nix] add fonts for zsh theme

--- nix/system/configuration.nix
+++ nix/system/configuration.nix
@@ -168,6 +168,10 @@
     libvirtd.enable = true;
   };
 
+  fonts.fonts = with pkgs; [
+    powerline-fonts
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
