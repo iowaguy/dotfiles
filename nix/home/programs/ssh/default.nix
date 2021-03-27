3c00dc3cc7485e882c4a60cfa3164e6fcd8bbe8c
Ben Weintraub <ben@weintraub.xyz>
2021-03-27 11:09:07 -0400

    [nix] use krb through jump host for open stack

--- nix/home/programs/ssh/default.nix
+++ nix/home/programs/ssh/default.nix
@@ -94,6 +94,20 @@ in {
           "ProxyCommand" = "ssh achtung -W %h:%p";
         };
       };
+      "jump.csail.mit.edu" = {
+        extraOptions = {
+          "GSSAPIAuthentication" = "yes";
+          "VerifyHostKeyDNS" = "yes";
+        };
+      };
+      "*.csail.mit.edu !jump.csail.mit.edu 128.52.* 128.30.* 128.31.*" =
+        dag.entryAfter [ "jump.csail.mit.edu" ] {
+          extraOptions = {
+            "ProxyJump" = "blw@jump.csail.mit.edu";
+            "GSSAPIAuthentication" = "yes";
+            "GSSAPIDelegateCredentials" = "yes";
+          };
+        };
     };
   };
 }
