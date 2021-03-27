c2ce333093ed076e2d614ce5f36d25eb5b936f75
Ben Weintraub <ben@weintraub.xyz>
2021-03-27 11:42:31 -0400

    [nix] condense ssh config

--- nix/home/programs/ssh/default.nix
+++ nix/home/programs/ssh/default.nix
@@ -9,89 +9,14 @@ in {
       "achtung" = {
         extraOptions = {
           "Hostname" = "achtung.ccs.neu.edu";
-          "Port" = "22";
           "User" = "bweintraub";
-          "ForwardAgent" = "yes";
         };
       };
-      "zion01" = {
+      "zion*" = dag.entryAfter [ "achtung" ] {
         extraOptions = {
-          "Hostname" = "zion01";
+          "ProxyJump" = "achtung";
           "User" = "bweintraub";
           "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion02" = {
-        extraOptions = {
-          "Hostname" = "zion02";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion03" = {
-        extraOptions = {
-          "Hostname" = "zion03";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion04" = {
-        extraOptions = {
-          "Hostname" = "zion04";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion05" = {
-        extraOptions = {
-          "Hostname" = "zion05";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion06" = {
-        extraOptions = {
-          "Hostname" = "zion06";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion07" = {
-        extraOptions = {
-          "Hostname" = "zion07";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion08" = {
-        extraOptions = {
-          "Hostname" = "zion08";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion09" = {
-        extraOptions = {
-          "Hostname" = "zion09";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
-        };
-      };
-      "zion10" = {
-        extraOptions = {
-          "Hostname" = "zion10";
-          "User" = "bweintraub";
-          "ForwardAgent" = "yes";
-          "ProxyCommand" = "ssh achtung -W %h:%p";
         };
       };
       "jump.csail.mit.edu" = {
