07f6e98534675b02168cbd54e07db2fe1ed9b1bd
Ben Weintraub <ben@weintraub.xyz>
2021-10-06 17:01:06 -0400

    [niv] write niv updater script

--- update.py
+++ update.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i python3 -p python3
+#! nix-shell -i python3 -p python3 niv
 
 import json
+import subprocess
 
+sources_path = "nix/sources.json"
 
-# read nix/sources.json
-# for each key
-#   run: niv update <key> -b <branch>
+if __name__ == "__main__":
+    with open(sources_path, 'r') as f:
+        sources_dict = json.load(f)
+
+    for key, val in sources_dict.items():
+        subprocess.run(["niv", "update", key, "-b", val['branch']])
