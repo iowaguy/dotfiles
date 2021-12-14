2f67fdaf1cfb3fe38bc4f97e91f1235e742e61a8
Ben Weintraub <ben@weintraub.xyz>
2021-12-14 14:58:09 -0500

    [wm] link per-machine i3 configs

--- home/programs/i3/default.nix
+++ home/programs/i3/default.nix
@@ -2,4 +2,6 @@
 
 {
   xdg.configFile."i3/config".source = ./i3-config.ini;
+  xdg.configFile."i3/isec-desktop-config.ini".source = ./isec-desktop-config.ini;
+  xdg.configFile."i3/kansas-config.ini".source = ./kansas-config.ini;
 }
