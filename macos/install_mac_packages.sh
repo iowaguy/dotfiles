7550cb5eaa5bbf968333ff71ad2fc9949c337f3f
Ben Weintraub <ben@weintraub.xyz>
2021-12-28 13:37:55 -0500

    [refactor] remove all old macos references

--- macos/install_mac_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Install the CLI tools for Xcode, must be done before installing Homebrew
-sudo xcode-select —-install
-
-echo "If previous command fails, install xcode manually from: https://developer.apple.com/download/more/"
-
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
--- macos/zsh/dot-zshrc.d/utils.zsh
+++
@@ -1,9 +0,0 @@
-# -*- mode: sh -*-
-
-function countpage() {
-  pdf2dsc "$1" /dev/stdout | grep "Pages" | sed s/[^0-9]//g
-}
-
-function path() {
-  echo $PATH | tr ':' '\n'
-}
