7df6f4d62bce79e25d1aa74b5c665e23548facd9
Ben Weintraub <ben@weintraub.xyz>
2022-01-03 15:21:58 -0500

    [polybar] remove missing network module

--- home/modules/polybar/top.nix
+++ home/modules/polybar/top.nix
@@ -1,7 +1,7 @@
 {
-  modules-right = "cpu memory temperature network pulseaudio battery date";
   background = "\${common.background}";
   foreground = "\${common.foreground}";
+  modules-right = "cpu memory temperature pulseaudio battery date";
   height = 50;
   dpi = 192;
   font-0 = "Droid Sans Mono for Powerline:style=Regular";
