d3f13fcada91bbd3c330177742801cbe73c5670c
Ben Weintraub <ben@weintraub.xyz>
2022-01-03 15:01:19 -0500

    [polybar] remove reference to unused module

--- home/modules/polybar/top.nix
+++ home/modules/polybar/top.nix
@@ -1,7 +1,7 @@
 {
   # background = "\${common.background}";
   # foreground = "\${common.foreground}";
-  modules-right = "cpu memory filesystem temperature wireless-network pulseaudio battery date";
+  modules-right = "cpu memory temperature network pulseaudio battery date";
   height = 50;
   dpi = 192;
   font-0 = "Droid Sans Mono for Powerline:style=Regular";
