e036d3a9600ca88e30642cd426a8985952c4640f
Ben Weintraub <ben@weintraub.xyz>
2022-01-04 10:39:24 -0500

    [polybar] clean up pulseaudio module

--- home/modules/polybar/modules/pulseaudio.nix
+++ home/modules/polybar/modules/pulseaudio.nix
@@ -1,30 +1,32 @@
 {
   type = "internal/pulseaudio";
 
+  # TODO This isn't working
+  click-right = "pavucontrol &";
+
   sink = "alsa_output.usb-Focusrite_Scarlett_2i4_USB-00.analog-surround-40";
 
-  format-volume = "<label-volume> <bar-volume>";
-  label-volume = "VOL %percentage%%";
-  # label-volume-foreground = "\${color.mb}";
-  # label-volume-background = "\${color.mb}";
+  # format-volume = "<label-volume> <bar-volume>";
+  format-volume = "<label-volume>";
+  label-volume = "🔊 %percentage%%  ";
 
-  label-muted = "🔇 muted";
+  label-muted = "🔇 muted  ";
   label-muted-foreground = "#666";
 
-  bar-volume-width = 10;
-  bar-volume-foreground-0 = "#55aa55";
-  bar-volume-foreground-1 = "#55aa55";
-  bar-volume-foreground-2 = "#55aa55";
-  bar-volume-foreground-3 = "#55aa55";
-  bar-volume-foreground-4 = "#55aa55";
-  bar-volume-foreground-5 = "#f5a70a";
-  bar-volume-foreground-6 = "#ff5555";
-  bar-volume-gradient = false;
-  bar-volume-indicator = "|";
-  bar-volume-indicator-font = 2;
-  bar-volume-fill = "─";
-  bar-volume-fill-font = 5;
-  bar-volume-empty = "─";
-  bar-volume-empty-font = 5;
-  bar-volume-empty-foreground = "\${color.brown}";
+  # bar-volume-width = 10;
+  # bar-volume-foreground-0 = "#55aa55";
+  # bar-volume-foreground-1 = "#55aa55";
+  # bar-volume-foreground-2 = "#55aa55";
+  # bar-volume-foreground-3 = "#55aa55";
+  # bar-volume-foreground-4 = "#55aa55";
+  # bar-volume-foreground-5 = "#f5a70a";
+  # bar-volume-foreground-6 = "#ff5555";
+  # bar-volume-gradient = false;
+  # bar-volume-indicator = "|";
+  # bar-volume-indicator-font = 3;
+  # bar-volume-fill = "─";
+  # bar-volume-fill-font = 5;
+  # bar-volume-empty = "─";
+  # bar-volume-empty-font = 5;
+  # bar-volume-empty-foreground = "\${color.brown}";
 }
