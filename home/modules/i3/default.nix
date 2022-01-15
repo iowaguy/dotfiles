f5e69c477cfd423a0eeec1dadecfdb9d18c6cb3f
Ben Weintraub <ben@weintraub.xyz>
2022-01-15 17:11:49 -0500

    [wm] add laptop i3 configs

--- home/modules/i3/default.nix
+++ home/modules/i3/default.nix
@@ -4,4 +4,5 @@
   xdg.configFile."i3/config".source = ./i3-config.ini;
   xdg.configFile."i3/isec-desktop-config.ini".source = ./isec-desktop-config.ini;
   xdg.configFile."i3/kansas-config.ini".source = ./kansas-config.ini;
+  xdg.configFile."i3/x1-2021-config.ini".source = ./x1-2021-config.ini;
 }
