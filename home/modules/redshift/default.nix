b9a962f82a19b923d662b36e6f14b032d3b2d307
Ben Weintraub <ben@weintraub.xyz>
2022-06-05 09:09:55 -0500

    [redshift] Dim at night

--- home/modules/redshift/default.nix
+++ home/modules/redshift/default.nix
@@ -5,5 +5,9 @@
       temperature.day = 6500;
       temperature.night = 3000;
       tray = true;
+      settings = {
+        redshift.brightness-day = 1.0;
+        redshift.brightness-night = 0.25;
+      };
     };
 }
