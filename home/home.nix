5e540acde3ef655fb0103bdd4b421a91ffb5830b
Ben Weintraub <ben@weintraub.xyz>
2022-07-10 06:53:17 -0500

    [bars] Remove taffybar deps from home

--- home/home.nix
+++ home/home.nix
@@ -42,11 +42,6 @@ let
     spotify                       # Musics
     sqlite
     stack                         # Haskell build tool
-
-    taffybar                      # A cool status bar
-    haskellPackages.gtk-sni-tray         # gtk-sni-tray-standalone, needed for taffybar https://github.com/taffybar/taffybar/issues/355
-    haskellPackages.status-notifier-item # status-notifier-watcher for, needed for taffybar https://github.com/taffybar/taffybar/issues/355
-
     texlive.combined.scheme-full
     tree
     vivaldi
