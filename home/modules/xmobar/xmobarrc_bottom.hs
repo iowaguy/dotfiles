eec858b5f8eef86d51f13d6f8edd7d037e83ea8b
Ben Weintraub <ben@weintraub.xyz>
2022-11-05 08:58:30 -0400

    [xmobar] Formatting

--- home/modules/xmobar/xmobarrc_bottom.hs
+++ home/modules/xmobar/xmobarrc_bottom.hs
@@ -5,7 +5,7 @@ Config
   -- , position = BottomP 0 800 -- align left, with 800 pixel space on the right for the tray
   -- , position = Static { xpos = 0, ypos = 0, width = 1346, height = 20 }
   -- , template = "%XMonadLog%}{"
-  , template = "%XMonadLog2%"
+  , template = "%XMonadLog2%}{"
   , commands =
     [
       Run NamedXPropertyLog "_XMONAD_LOG_2" "XMonadLog2"
--- home/modules/xmobar/xmobarrc_top.hs
+++ home/modules/xmobar/xmobarrc_top.hs
@@ -1,23 +1,24 @@
 Config
   {
-    font = "xft:iosevka:size=11:bold:antialias=true"
+    font = "xft:iosevka:size=14:bold:antialias=true"
   , position = Top
-  -- , position = BottomP 0 800 -- align left, with 800 pixel space on the right for the tray
+  -- , position = TopP 0 800 -- align left, with 800 pixel space on the right for the tray
   -- , position = Static { xpos = 0, ypos = 0, width = 1346, height = 20 }
-  , template = "}%cpu%{%alsa:default:Master% (%wlp0s20f3wi%) %battery% %date%  "
+  , template = "}%cpu%  %memory% %alsa:default:Master%{(%wlp0s20f3wi%) %battery% %date%"
   , commands =
     [
-      Run Wireless "wlp0s20f3"
-      ["--template", "<ssid> <quality>%", "--"] 100
-    , Run Alsa "default" "Master"
+      Run Alsa "default" "Master"
       ["--template"
       , "<status> <volume>%", "--"
       , "-O", "♪"
       , "-o", "[muted]"
       ]
     , Run Cpu
-      ["--template", " <total>", "--"]
+      ["--template", " <total>%", "--"]
       100
+    , Run Memory [ "-t", " <usedratio>%", "-d", "1", "--", "--scale", "1024"] 100
+    , Run Wireless "wlp0s20f3"
+      ["--template", "<ssid> <quality>%", "--"] 100
     , Run BatteryP
       ["BAT0"]
       ["--template", "<acstatus>", "--"
