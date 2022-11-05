8c94d667d3e4697cbeafc871493c1eeea58ac292
Ben Weintraub <ben@weintraub.xyz>
2022-11-05 08:59:49 -0400

    [bars] Add stalonetray to xmobar

--- home/modules/xmobar/default.nix
+++ home/modules/xmobar/default.nix
@@ -2,9 +2,11 @@
 {
   home.packages = with pkgs; [
     # xmobar
+    stalonetray # a tray to hold app icons
   ];
   xdg.configFile."xmobar/xmobarrc_top".source = ./xmobarrc_top.hs;
   xdg.configFile."xmobar/xmobarrc_bottom".source = ./xmobarrc_bottom.hs;
 
   # xdg.configFile."xmobar/xmobarrc2".source = ./xmobarrc;
+  home.file.".stalonetrayrc".source = ./stalonetrayrc;
 }
