441101c00265d2a2324e8b59d263e0f51a353b27
Ben Weintraub <ben@weintraub.xyz>
2022-12-02 09:38:29 -0500

    [bars] Add brightness control

--- home/modules/xmobar/xmobarrc_top.hs
+++ home/modules/xmobar/xmobarrc_top.hs
@@ -4,7 +4,7 @@ Config
   , position = Top
   -- , position = TopP 0 800 -- align left, with 800 pixel space on the right for the tray
   -- , position = Static { xpos = 0, ypos = 0, width = 1346, height = 20 }
-  , template = "}%cpu%  %memory% %alsa:default:Master%{(%wlp0s20f3wi%) %battery% %date%"
+  , template = "}%cpu%  %memory% %alsa:default:Master% %bright% {(%wlp0s20f3wi%) %battery% %date%"
   , commands =
     [
       Run Alsa "default" "Master"
@@ -29,5 +29,8 @@ Config
       ,"-i", ""
       ] 100
     , Run Date "%A, %d %B %H:%M %Z" "date" 100
+    , Run Brightness ["--template", " <percent>%",
+                      "--", "-D", "intel_backlight"
+                     ] 100
     ]
 }
