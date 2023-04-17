cd772c87649f03c073d418c137fd464de5643f86
Ben Weintraub <ben@weintraub.xyz>
2023-04-17 09:27:47 -0400

    [xmobar] Configure font sizes after update

--- home/modules/xmobar/xmobarrc_bottom.hs
+++ home/modules/xmobar/xmobarrc_bottom.hs
@@ -1,12 +1,12 @@
 Config
   {
-    font = "xft:iosevka:size=20:bold:antialias=true"
-  , position = Bottom
+    font = "Iosevka Comfy Bold 25",
+    -- position = Bottom,
+    position = BottomH 30,
   -- , position = BottomP 0 800 -- align left, with 800 pixel space on the right for the tray
   -- , position = Static { xpos = 0, ypos = 0, width = 1346, height = 20 }
-  -- , template = "%XMonadLog%}{"
-  , template = "%XMonadLog2%}{"
-  , commands =
+    template = "%XMonadLog2%}{",
+    commands =
     [
       Run NamedXPropertyLog "_XMONAD_LOG_2" "XMonadLog2"
     ]
--- home/modules/xmobar/xmobarrc_top.hs
+++ home/modules/xmobar/xmobarrc_top.hs
@@ -1,12 +1,10 @@
 Config
   {
-    font = "xft:iosevka:size=14:bold:antialias=true"
-  , position = Top
-  -- , position = TopP 0 800 -- align left, with 800 pixel space on the right for the tray
-  -- , position = Static { xpos = 0, ypos = 0, width = 1346, height = 20 }
+    font = "Iosevka Comfy Bold 25",
+    position = TopH 30,
   -- , template = "}%cpu%  %memory% %alsa:default:Master% %bright% {(%wlp0s20f3wi%) %battery% %date%"
-  , template = "}%cpu%  %memory% %bright% {(%wlp0s20f3wi%) %battery% %date%"
-  , commands =
+    template = "}%cpu%  %memory% %bright% {(%wlp0s20f3wi%) %battery% %date%",
+    commands =
     [
       -- Run Alsa "default" "Master"
       -- ["--template"
@@ -16,11 +14,11 @@ Config
       -- ]
       Run Cpu
       ["--template", " <total>%", "--"]
-      100
-    , Run Memory [ "-t", " <usedratio>%", "-d", "1", "--", "--scale", "1024"] 100
-    , Run Wireless "wlp0s20f3"
-      ["--template", "<ssid> <quality>%", "--"] 100
-    , Run BatteryP
+      100,
+      Run Memory [ "-t", " <usedratio>%", "-d", "1", "--", "--scale", "1024"] 100,
+      Run Wireless "wlp0s20f3"
+      ["--template", "<ssid> <quality>%", "--"] 100,
+      Run BatteryP
       ["BAT0"]
       ["--template", "<acstatus>", "--"
       ,"-O", "<left>+ <fc=#aaffaa>|</fc>"
@@ -28,10 +26,12 @@ Config
       ,"-P"  -- Display percent symbol with <left>
       ,"-l", "red", "-m", "blue", "-h", "green"
       ,"-i", ""
-      ] 100
-    , Run Date "%A, %d %B %H:%M %Z" "date" 100
-    , Run Brightness ["--template", " <percent>%",
+      ] 100,
+      Run Date "%A, %d %B %H:%M %Z" "date" 100,
+      Run Brightness ["--template", " <percent>%",
                       "--", "-D", "intel_backlight"
-                     ] 100
+                     ] 100,
+
+      Run NamedXPropertyLog "_XMONAD_LOG_2" "XMonadLog2"
     ]
 }
