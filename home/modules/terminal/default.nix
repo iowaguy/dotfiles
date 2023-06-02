dc98f6fbc490e1b66715804360130b3884ee4080
Ben Weintraub <ben@weintraub.xyz>
2023-06-02 09:40:25 -0400

    [term] add some kitty options
    Increase font size, and add a reference to an option I will want in a
    future version.

--- home/modules/terminal/default.nix
+++ home/modules/terminal/default.nix
@@ -7,9 +7,13 @@
 
   programs.kitty = {
     enable = true;
+
+    # TODO enable this after upgading to 23.05 or possibly the next release
+    # shellIntegration.enableZshIntegration = true;
+
     # View all settings here: https://sw.kovidgoyal.net/kitty/conf/
     settings = {
-      font_size = 20;
+      font_size = 25;
       enable_audio_bell = false;
       scrollback_lines = 10000;
       strip_trailing_spaces = "smart";
