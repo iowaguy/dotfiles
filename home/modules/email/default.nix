29fd43e17ee88ac6c091489564045155be5127dd
Ben Weintraub <ben@weintraub.xyz>
2023-06-15 14:04:50 -0400

    [email] remove unused packages

--- home/modules/email/default.nix
+++ home/modules/email/default.nix
@@ -3,8 +3,6 @@
 let passCmd = entry: "${pkgs.pass}/bin/pass ${entry} 2> /dev/null";
 in {
   home.packages = with pkgs; [
-    afew # initial tagging for notmuch
-    notmuch # an email search engine
     mailspring
   ];
 
