b44d89f534ecb24311bfc9cdfb2651abcf2c3d76
Ben Weintraub <ben@weintraub.xyz>
2023-07-02 16:15:45 -0400

    [autorandr] setup home monitor and cleanup autoswitch

--- modules/home/autorandr/default.nix
+++ modules/home/autorandr/default.nix
@@ -9,43 +9,52 @@ let
     "HDMI-1" =
       "00ffffffffffff001e6d085b24020700091c0103803c2278ea3035a7554ea3260f50542108007140818081c0a9c0d1c081000101010108e80030f2705a80b0588a0058542100001e04740030f2705a80b0588a0058542100001a000000fd00383d1e873c000a202020202020000000fc004c4720556c7472612048440a2001db020330714d902220050403020161605d5e5f230907076d030c001000b83c20006001020367d85dc401788003e30f0006023a801871382d40582c450058542100001a565e00a0a0a029503020350058542100001a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000aa";
   };
+  fingerprintLGHome = {
+    "HDMI-1" =
+      "00ffffffffffff001e6d085b162c0000091c0103803c2278ea3035a7554ea3260f50542108007140818081c0a9c0d1c081000101010108e80030f2705a80b0588a0058542100001e04740030f2705a80b0588a0058542100001a000000fd00383d1e873c000a202020202020000000fc004c4720556c7472612048440a2001c6020330714d902220050403020161605d5e5f230907076d030c001000b83c20006001020367d85dc401788003e30f0006023a801871382d40582c450058542100001a565e00a0a0a029503020350058542100001a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000aa";
+  };
+
+  x1config = {
+    eDP-1 = {
+      enable = true;
+      primary = true;
+      mode = "3840x2400";
+      position = "0x0";
+      crtc = 0;
+    };
+  };
+
+  lgconfig = {
+    HDMI-1 = {
+      primary = true;
+      enable = true;
+      mode = "3840x2160";
+      crtc = 1;
+      position = "3840x0";
+      rate = "60.00";
+    };
+  };
 in
 {
   programs.autorandr = {
+    enable = true;
     hooks.postswitch = {
       "change-background" = "${pkgs.feh}/bin/feh --bg-fill ~/.background-image";
-      "restart-xmonad" = "${pkgs.xmonad}/bin/xmonad --restart";
+      "restart-xmonad" = "${pkgs.haskellPackages.xmonad}/bin/xmonad --restart";
     };
     profiles = {
       "unpluggedX1_2021" = {
         fingerprint = fingerprintX1_2021;
-        config = {
-          "eDP-1" = {
-            enable = true;
-            primary = true;
-            mode = "3840x2400";
-          };
-        };
+        config = x1config;
       };
       # configure manually with: `xrandr --output eDP-1 --auto --output HDMI-1 --right-of eDP-1`
-      "home_X1_2021" = {
+      "lab" = {
         fingerprint = fingerprintX1_2021 // fingerprintLGLab;
-        config = {
-          "eDP-1" = {
-            enable = true;
-            position = "0x0";
-            mode = "3840x2400";
-            crtc = 0;
-          };
-          "HDMI-1" = {
-            primary = true;
-            enable = true;
-            mode = "3840x2160";
-            crtc = 1;
-            pos = "3828x0";
-            rate = 60.00;
-          };
-        };
+        config = x1config // lgconfig;
+      };
+      "home" = {
+        fingerprint = fingerprintX1_2021 // fingerprintLGHome;
+        config = x1config // lgconfig;
       };
     };
   };
