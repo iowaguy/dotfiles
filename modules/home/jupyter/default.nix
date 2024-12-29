f82c1f8c2774a49075ae0730a04bfd992dbd508d
Ben Weintraub <ben@weintraub.xyz>
2024-12-29 16:54:24 -0500

    [jupyter] Refactoring

--- modules/home/jupyter/default.nix
+++ modules/home/jupyter/default.nix
@@ -1,11 +1,10 @@
 { pkgs, pkgsUnstable, ... }:
 
 {
-  home.packages = with pkgsUnstable; [
-    # python311
-    python311Packages.pandas
-    python311Packages.numpy
-    python311Packages.pymongo
-    python311Packages.seaborn
+  home.packages = with pkgsUnstable.python311Packages; [
+    pandas
+    numpy
+    pymongo
+    seaborn
   ];
 }
