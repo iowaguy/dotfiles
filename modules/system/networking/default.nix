2ff8ccfa80ef361d1efd6c10d01f172e11102a0c
Ben Weintraub <ben@weintraub.xyz>
2025-05-25 10:59:19 -0400

    [web] Block distracting websites

--- modules/system/networking/default.nix
+++ modules/system/networking/default.nix
@@ -4,11 +4,18 @@
     # false here.
     useDHCP = false;
 
-    # # Block sites that distract me
-    # extraHosts = ''
-    #   127.0.0.1 nytimes.com
-    #   127.0.0.1 news.ycombinator.com
-    # '';
+    # Block sites that distract me
+    extraHosts = ''
+      127.0.0.1 www.nytimes.com
+      127.0.0.1 news.ycombinator.com
+      127.0.0.1 2048.org
+      127.0.0.1 www.youtube.com
+      127.0.0.1 youtube-ui.l.google.com
+      127.0.0.1 googlevideo.com
+      127.0.0.1 apnews.com
+      127.0.0.1 cnn.com
+    '';
+
     firewall.allowedTCPPorts = [
       22067 # syncthing
       36885 # syncthing
