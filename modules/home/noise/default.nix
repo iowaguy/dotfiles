8a2df9d38a0f9713ed136e8839365796c31d15a4
Ben Weintraub <ben@weintraub.xyz>
2025-05-25 11:00:32 -0400

    [noise] Clean up white noise tool

--- modules/home/noise/default.nix
+++ modules/home/noise/default.nix
@@ -1,8 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.file.".bin/noise" = {
-    source = ./noise;
-    executable = true;
+  home.file = {
+    ".bin/noise" = {
+      source = ./noise;
+      executable = true;
+    };
+    ".bin/noise-control" = {
+      source = ./noise-control.sh;
+      executable = true;
+    };
   };
 }
