1f0a715b79b6ca8a09823fbe25f627a32947e9d0
Ben Weintraub <ben@weintraub.xyz>
2025-08-04 18:06:52 -0400

    [slack] Remove unused desktop file

--- modules/home/slack/default.nix
+++ modules/home/slack/default.nix
@@ -5,16 +5,6 @@
     slack
   ];
 
-  home.file.".local/share/applications/slack.desktop".text = ''
-    [Desktop Entry]
-    Encoding=UTF-8
-    Version=1.0
-    Type=Application
-    Terminal=false
-    Exec=${config.home.profileDirectory}/bin/slack
-    Name=slack
-  '';
-
   xdg.mimeApps = {
     associations.added = {
       "x-scheme-handler/slack" = [ "slack.desktop" ];
