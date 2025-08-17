09f5e3b235514b15f63a9a5c4f85085d84929566
Ben Weintraub <ben@weintraub.xyz>
2025-08-17 08:17:57 -0400

    [wireguard] Working wg config for acadia

--- modules/system/wireguard/default.nix
+++ modules/system/wireguard/default.nix
@@ -1,21 +1,18 @@
 {
   networking = {
-    firewall.allowedUDPPorts = [ 51820 ];
-    wireguard = {
-      enable = true;
+    wg-quick = {
       interfaces = {
         wg0 = {
-          ips = [ "10.100.0.2/24" ];
+          address = [ "192.168.100.2/32" ];
           privateKeyFile = "/home/ben/wireguard-keys/private";
-          listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+          autostart = true;
           peers = [
             {
-              name = "bens-laptop";
               publicKey = "2POqMTQ7ozHbAbGo06h7clIqAEHnMOndCfOz0WSdm2k=";
-              endpoint = "129.10.186.241:51820";
-              allowedIPs = [ "10.100.0.0/24" ];
+              endpoint = "acadia.ben-weintraub.com:51820";
+              allowedIPs = [ "192.168.100.0/24" ];
               persistentKeepalive = 25;
-              # presharedKeyFile = "/home/ben/wireguard-keys/presharedkey.psk";
+              presharedKeyFile = "/home/ben/wireguard-keys/presharedkey.psk";
             }
           ];
         };
