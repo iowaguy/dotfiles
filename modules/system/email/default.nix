38dbf12afba8a98586af2b58c19fdab28d3ef5af
Ben Weintraub <ben@weintraub.xyz>
2025-09-14 17:09:41 -0400

    [email] nextcloud first draft

---
+++ modules/system/email/default.nix
@@ -0,0 +1,63 @@
+{ self, config, lib, pkgs, ... }:
+{
+  services = {
+    nginx.virtualHosts = {
+      "email.ben-weintraub.com" = {
+        forceSSL = true;
+        enableACME = true;
+      };
+
+      "nextcloud.ben-weintraub.com" = {
+        forceSSL = true;
+        enableACME = true;
+      };
+    };
+
+    nextcloud = {
+      enable = true;
+      hostName = "email.ben-weintraub.com";
+
+       # Need to manually increment with every major upgrade.
+      package = pkgs.nextcloud31;
+
+      # Let NixOS install and configure the database automatically.
+      database.createLocally = true;
+
+      # Let NixOS install and configure Redis caching automatically.
+      configureRedis = true;
+
+      # Increase the maximum file upload size to avoid problems uploading videos.
+      maxUploadSize = "16G";
+      https = true;
+      enableBrokenCiphersForSSE = false;
+
+      autoUpdateApps.enable = true;
+      extraAppsEnable = true;
+      extraApps = with config.services.nextcloud.package.packages.apps; {
+        # List of apps we want to install and are already packaged in
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+        inherit calendar contacts mail notes onlyoffice tasks;
+
+        # Custom app installation example.
+        cookbook = pkgs.fetchNextcloudApp rec {
+          url =
+            "https://github.com/nextcloud/cookbook/releases/download/v0.10.2/Cookbook-0.10.2.tar.gz";
+          sha256 = "sha256-XgBwUr26qW6wvqhrnhhhhcN4wkI+eXDHnNSm1HDbP6M=";
+        };
+      };
+
+      config = {
+        overwriteProtocol = "https";
+        defaultPhoneRegion = "ET";
+        dbtype = "pgsql";
+        adminuser = "admin";
+        adminpassFile = ./nextcloud-admin-pass;
+      };
+    };
+
+    onlyoffice = {
+      enable = true;
+      hostname = "nextcloud.ben-weintraub.com";
+    };
+  };
+}
